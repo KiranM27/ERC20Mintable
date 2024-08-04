64f8d3075c48f5da09dae2bd20db0e6483fec53b
Kiran Mohan <58102994+KiranM27@users.noreply.github.com>
2024-08-04 16:22:54 +0800

    feat: deployed node erc 20 contract to the sepolia testnet

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,9 +1,23 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-ignition";
+import "dotenv/config";
+
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""; // to verify the contract on Etherscan
+const SEPOLIA_DEPLOYER_PRIVATE_KEY = process.env.DEPLOYER_PRIVATE_KEY || "";
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_ETH_TESTNET_RPC_URL;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [SEPOLIA_DEPLOYER_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
 };
 
 export default config;
