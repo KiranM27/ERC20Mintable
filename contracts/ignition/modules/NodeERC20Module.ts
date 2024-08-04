567ac235f199359c00a4de779682c708cbbefbff
Kiran Mohan <58102994+KiranM27@users.noreply.github.com>
2024-08-04 16:06:42 +0800

    chore: delete ignition
    going back to using scripts for deploying the contracts

--- contracts/ignition/modules/NodeERC20Module.ts
+++
@@ -1,9 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-export default buildModule("NodeERC20Module", (m) => {
-  const deployer = m.getAccount(0);
-  // get the deployer address
-  const token = m.contract("NodeERC20", [deployer, deployer, deployer]);
-
-  return { token };
-});
