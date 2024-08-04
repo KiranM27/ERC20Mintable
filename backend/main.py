43e22897e87c97f7871f8720b6a898e9909a2569
Kiran Mohan <58102994+KiranM27@users.noreply.github.com>
2024-08-04 19:47:58 +0800

    feat: script to run the backend

--- backend/main.py
+++ backend/main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 async def hello_world():
     return {"message": "Hello World"}
+
+@app.get("/get_token_info")
+async def get_token_info():
+    return {"token": "token_info"}
---
+++ backend/run.sh
@@ -0,0 +1,3 @@
+# a bash script to run the backend server 
+# since the uvicorn server is a bit complicated to remember
+uvicorn main:app --reload
